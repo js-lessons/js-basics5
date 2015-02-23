358e7a1fc42ac6833e964f6655f668870f1221d9
Kate Sazonova <sazonova.k.y@gmail.com>
2015-02-23 22:15:26 +0200

    done

--- solution/stretchCell.js
+++ solution/stretchCell.js
@@ -1,9 +1,17 @@
 function StretchCell(inner, width, height) {
-  // Implement a cell type named StretchCell(inner, width, height)
-  // that conforms to the table cell interface described in example inside vendor folder.
-  // It should wrap another cell (like UnderlinedCell does)
-  // and ensure that the resulting cell has at least the given
-  // width and height, even if the inner cell would naturally be smaller.
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height);
 };
 
 module.exports = StretchCell;
